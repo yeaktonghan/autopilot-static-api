8113da12d7632856f914b159e4868eff9ea5ed25
KSGA-Autopilot <kshrdautopilot@gmail.com>
2023-10-25 16:36:37 +0700

    change branch field

--- src/main/java/com/kshrd/autopilot/entities/dto/DeploymentAppDto.java
+++ src/main/java/com/kshrd/autopilot/entities/dto/DeploymentAppDto.java
@@ -1,6 +1,5 @@
 package com.kshrd.autopilot.entities.dto;
 
-import com.kshrd.autopilot.entities.ProjectBranch;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
