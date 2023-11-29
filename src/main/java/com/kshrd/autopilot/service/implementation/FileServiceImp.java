da7af6889ef36ed6fb96e7fad5937720f4aa0db1
KSGA-Autopilot <kshrdautopilot@gmail.com>
2023-11-29 10:23:24 +0700

    change port argo to 31142

--- src/main/java/com/kshrd/autopilot/service/implementation/FileServiceImp.java
+++ src/main/java/com/kshrd/autopilot/service/implementation/FileServiceImp.java
@@ -35,8 +35,8 @@ public class FileServiceImp implements FileService {
         obj.setFileType(file.getContentType());
         obj.setSize(file.getSize());
         URL url = new URL(String.valueOf(request.getRequestURL()));
-        String baseUrl = url.getProtocol() + "://" + url.getHost() + "/";
-        obj.setFileUrl(baseUrl+":8080/"+"api/v1/file?file="+obj.getFileName());
+        String baseUrl = url.getProtocol() + "://" + url.getHost()+":8080"+ "/";
+        obj.setFileUrl(baseUrl+"api/v1/file?file="+obj.getFileName());
         String uploadPath = fileStorageProperties.getUploadPath();
         Path directoryPath = Paths.get(uploadPath).toAbsolutePath().normalize();
         java.io.File directory = directoryPath.toFile();
